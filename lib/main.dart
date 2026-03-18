963b5bc6ada189902836e3b55f7b3df07910f7b5
S@V@B@F <34383710+Edembf@users.noreply.github.com>
2026-03-18 23:39:54 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,13 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: const FirebaseOptions (
-    apiKey: "AIzaSyDPzwY80-oZCo5bYoaHAoA0x-3vwaLU7dA",
-    authDomain: "moviewatcher-app-9388f.firebaseapp.com",
-    projectId: "moviewatcher-app-9388f",
-    storageBucket: "moviewatcher-app-9388f.firebasestorage.app",
-    messagingSenderId: "168780394464",
-    appId: "1:168780394464:web:acc8cac29f4277d3b24585",
-    measurementId: "G-ZV8PDPCR6L"
+    apiKey: "XXXX",
+    authDomain: "XXXX",
+    projectId: "XXXX",
+    storageBucket: "XXXX",
+    messagingSenderId: "XXXX",
+    appId: "XXXX",
+    measurementId: "XXXX"
       ),
   );
 
